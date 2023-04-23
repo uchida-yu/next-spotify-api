888cdc72d75f1cabcd2fe5d687c97860ecdc9e37
uchida-yu <yu3shabadaba+github@gmail.com>
2023-04-24 00:09:44 +0900

    修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
+  /* eslint-disable react/jsx-props-no-spreading */
   return (
     <RecoilRoot>
       <Component {...pageProps} />
